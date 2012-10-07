8dd10026d31cb631cecf81c70eb60e9572b70c2d
Anatoly Shulipov <itolianezzz@gmail.com>
2012-10-07 12:51:26 +0400

    some more changes

---
+++ src/ru/spb/itolia/perashki/adapters/PiroAdapter.java
@@ -0,0 +1,50 @@
+package ru.spb.itolia.perashki.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import ru.spb.itolia.perashki.R;
+import ru.spb.itolia.perashki.beans.Piro;
+
+import java.util.List;
+
+/**
+* Created with IntelliJ IDEA.
+* User: itolia
+* Date: 07.10.12
+* Time: 12:14
+*/
+public class PiroAdapter extends ArrayAdapter<Piro> {
+    private Context context;
+    private List<Piro> piros;
+
+    public PiroAdapter(Context context, List<Piro> piros) {
+        super(context, R.layout.piro_list_item, R.id.piro_text, piros);
+        this.context = context;
+        this.piros = piros;
+    }
+
+    class ViewHolder {
+        private TextView piro_text;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        if (rowView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.piro_list_item, null);
+            ViewHolder viewHolder = new ViewHolder();
+            rowView.setTag(viewHolder);
+        }
+        ViewHolder holder = (ViewHolder) rowView.getTag();
+        holder.piro_text = (TextView) rowView.findViewById(R.id.piro_text);
+        Piro p = (Piro) piros.get(position);   //TODO change for RedmineSession
+        holder.piro_text.setText(p.getText());
+        return rowView;
+    }
+}
--- src/ru/spb/itolia/perashki/ui/PiroListFragment.java
+++ src/ru/spb/itolia/perashki/ui/PiroListFragment.java
@@ -1,12 +1,19 @@
 package ru.spb.itolia.perashki.ui;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockListFragment;
+import ru.spb.itolia.perashki.PiroLoader;
 import ru.spb.itolia.perashki.R;
+import ru.spb.itolia.perashki.adapters.PiroAdapter;
+import ru.spb.itolia.perashki.beans.Piro;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,15 +21,40 @@ import ru.spb.itolia.perashki.R;
  * Date: 06.10.12
  * Time: 15:24
  */
-public class PiroListFragment extends SherlockFragment {
+public class PiroListFragment extends SherlockListFragment {
 
     private ListView list;
+
+    public PiroListFragment() {
+        }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.piro_list_view, container, false);
-        list = (ListView) view.findViewById(R.id.piro_list);
-
+        //list = (ListView) view.findViewById(R.id.list);
+        LoadPirosTask pirosTask = new LoadPirosTask();
+        pirosTask.execute();
         return view;
     }
+
+    private class LoadPirosTask extends AsyncTask<Void, Void, List<Piro>> {
+
+        @Override
+        protected List<Piro> doInBackground(Void... params) {
+            List<Piro> piros = null;
+            try {
+                piros = PiroLoader.getGood();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            return piros;
+        }
+
+        @Override
+        protected void onPostExecute(List<Piro> piros){
+            PiroAdapter mAdapter = new PiroAdapter(getActivity(), piros);
+            setListAdapter(mAdapter);
+        }
+    }
+
 }
